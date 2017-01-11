8aa92f87c0f30c5609515a595fb0a46134adbb29
windwardfrc <shuss@windwardschool.org>
2017-01-10 16:23:22 -0800

    Add comment from origin

--- src/TestClass.java
+++ src/TestClass.java
@@ -3,4 +3,6 @@ public class TestClass {
 	// Hello there, Windward github master!
 	
 	// This is a change. Do you see it upstream? 
+	
+	// This is a change from origin. Do you see it downstream? 
 }
